6892bc7d03abb9b4130b63e204271957d0ebce0e
luntan <lucas.orman@gmail.com>
2022-06-21 11:12:08 -0500

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles.css'
 import { UserProvider } from '@auth0/nextjs-auth0'
-
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
     return (
     <UserProvider>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script strategy="lazyOnload">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                });
+            `}
+        </Script>
         <Component {...pageProps} />
     </UserProvider>
     )
